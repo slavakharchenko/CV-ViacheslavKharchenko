ce81a766dff318ef9c8a61548827bd47884ab7df
Viacheslav <viacheslav.kharchenko@pm.bet>
2022-03-07 18:41:57 +0200

    Delete CV

---
+++ src/components/body/skill/skill.style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin-top: 160px;
+
+  padding-bottom: 120px;
+
+  margin-left: 70px;
+  margin-right: 70px;
+  
+  color: black;
+`;
+
+export const SkillWrapper = styled.div`
+  display: flex;
+  grid-template-columns: 1.5fr 1fr;
+  grid-template-areas: "content photo";
+  grid-column-gap: 20px;
+  
+  margin-top: 120px;
+  
+  padding-top: 120px;
+  padding-bottom: 120px;
+  
+  margin-left: 70px;
+  margin-right: 70px;
+`
